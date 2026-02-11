3b34796d092569e00b6a6efc561eeb12c9c52197
Bahar <baharalekberova05gmail.com>
2026-02-11 12:00:51 +0400

    added nested layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,20 @@
-import Navigation from "./components/Navigation";
-import Logo from "./components/Logo";
+import Header from "@/app/_components/Header";
+import "@/app/_styles/globals.css";
+import { Josefin_Sans } from "next/font/google";
+
+const josefin = Josefin_Sans({
+  subsets: ["latin"],
+  display: "swap",
+});
 
 export const metadata = {
-  title: "The Wild Oasis",
+  // title: "The Wild Oasis",
+  title: {
+    template: "%s / The Wild Oasis",
+    default: "Welcome / The Wild Oasis",
+  },
+  description:
+    "Luxurios cabin hotel, located in the heart of the Italian Dolomites surrounded by beatuful mountains and dark forests.",
 };
 
 export default function RootLayout({
@@ -12,13 +24,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>
-        <header>
-          <Logo />
-          <Navigation />
-        </header>
-        <main>{children}</main>
-        <footer>Copyright by Bahar Alekberova</footer>
+      <body
+        className={`${josefin.className} antialiased min-h-screen bg-primary-950 text-primary-100 flex flex-col relative`}
+      >
+        <Header />
+        <div className="flex-1 px-8 py-12 grid">
+          <main className="max-w-7xl mx-auto w-full">{children}</main>
+        </div>
       </body>
     </html>
   );
